10d43c24ce4f944daeca185feefff8a62807291d
Roman Likhomanov <romanlee87@gmail.com>
2022-10-31 11:25:38 +0300

    Updates

---
+++ precache-manifest.fd9452bef3bb0521deb6d60418986dd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b284f59e3db148fd6a8c37d9475af9",
+    "url": "/todo-toolkit/index.html"
+  },
+  {
+    "revision": "e7a44c016c917a0eb134",
+    "url": "/todo-toolkit/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/todo-toolkit/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todo-toolkit/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a44c016c917a0eb134",
+    "url": "/todo-toolkit/static/js/main.0b56eb2f.chunk.js"
+  },
+  {
+    "revision": "a59cc9b91405debb8ad7",
+    "url": "/todo-toolkit/static/js/runtime-main.1be145f3.js"
+  }
+]);
